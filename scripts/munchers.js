e43afc6cb7518b0de552b5373bf3bbb7f1551969
ben switzer <sbs14d@my.fsu.edu>
2016-03-25 21:09:27 -0500

    Gridwork has been put in place.

--- scripts/munchers.js
+++ scripts/munchers.js
@@ -4,6 +4,134 @@
 // pollution of the global scope.
 var munchers = {};
 
+munchers.createMonster = function(){ return {}; };
+
+munchers.createGrid = function () {
+  // Height and Width of the grid.
+  var GRID_HEIGHT = 6;
+  var GRID_WIDTH = 5;
+  
+  // the grid array
+  var _grid = [];
+
+  // The number the player will have to find multiples of.
+  var _number; 
+  
+ return {
+    // This function is used to generate a monster onto the board
+    // from a corner of the board.
+    generateMonster: function () {
+        // position to generate troggle
+        var h = 0, w = 0;
+        
+
+        var corner = parseInt(Math.random() * 4);
+        
+        // 0 = top line
+        if(corner === 0)
+        {
+          w = parseInt(Math.random() * GRID_WIDTH);
+        }
+        // 1 = left line
+        else if(corner === 1)
+        {
+          h = parseInt(Math.random() * GRID_HEIGHT);
+        }
+         // 2 = bottom line
+        else if(corner === 2)
+        {
+          h = GRID_HEIGHT - 1;
+          w = parseInt(Math.random() * GRID_WIDTH);
+        }
+        // 3 = right line 
+        else 
+        {
+          w = GRID_WIDTH - 1;
+          h = parseInt(Math.random() * GRID_HEIGHT);
+        }
+
+        // if an object isn't already in place, then place a monster.
+        if(typeof _grid[w][h] !== "object")
+        {
+          _grid[w][h] = munchers.createMonster();
+        }        
+    },
+
+    // Generate a number between 2 and 12
+    generateMultiple: function(){
+      _number = parseInt((Math.random() * 10) + 2);
+    },
+
+    // Returns the grid object.
+    grid: function () { 
+      return _grid; 
+    },
+
+    // fills the board with numbers, and producdes multiples of said numbers. 
+    fill: function () {
+        for(var w = 0; w < GRID_WIDTH; ++w)
+        {
+          _grid[w] = [];
+          for(var h = 0; h < GRID_HEIGHT; ++h){
+
+            // assign a random number to c.
+            var c = parseInt(Math.random() * 5);
+           
+            // take the number and apply a multipler. 
+            var multiplied = _number * parseInt((1 + (Math.random() * 5)));
+            
+            // so if the value of c is 3, then we will 
+            // a) assign the grid position to the multiplied number, or
+            // b) take the multipled number and add a random number to it. 
+            _grid[w][h] = parseInt( c % 3 == 0 ? 
+              multiplied            
+              : (multiplied + ((Math.random() + 1) * 11 ) )
+              );  
+
+          } // end of height for loop.
+        } // end of width for loop.
+    },
+
+    // return the number we are looking for multiples of.
+    number: function () {
+      return _number;
+    },
+
+    // Clears the grid.
+    reset: function () {
+      for(var w = 0; w < GRID_WIDTH; ++w)
+        for(var h = 0; h < GRID_HEIGHT; ++h)
+          _grid[w][h] = '';
+    },
+
+    /*
+     *   Debug Methods
+     */
+
+
+    // Prints out a current display of the grid.
+    debug: function()
+    {
+      console.log("Grid dimensions: " + GRID_WIDTH + " by " + GRID_HEIGHT + "\n");
+      console.log("     Multiples of " + _number + "\n");
+       for(var h = 0; h < GRID_HEIGHT; ++h)
+       {
+        var g = "";
+        for(var w = 0; w < GRID_WIDTH; ++w)
+        {
+          if(typeof _grid[w][h] !== "object")
+            g += _grid[w][h] + (_grid[w][h] < 10 ? "    " : "   ");
+          else 
+            g += "T    ";
+        }
+        console.log(g + "\n");
+      }
+    }
+
+  } // end of grid
+} // end of createGrid
+
+
 // Creates an encapsulated leaderboard object.
 munchers.createLeaderBoard = function () {
   var MAX_NUM_HIGH_SCORES = 10;
@@ -81,3 +209,5 @@ munchers.createLeaderBoard = function () {
 }  // end of createLeaderBoard()
 
 munchers.leaderBoard = munchers.createLeaderBoard();
+munchers.grid = munchers.createGrid();
+
